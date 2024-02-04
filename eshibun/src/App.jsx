998a6aed45e076f852b1f787df3bb490fd282539
Louan Flammanc <kusami.louan@gmail.com>
2024-02-04 17:28:08 -0500

    2

--- eshibun/src/App.jsx
+++ eshibun/src/App.jsx
@@ -1,17 +1,30 @@
-import { Canvas, useFrame } from "@react-three/fiber"
+import { Canvas, events, useFrame } from "@react-three/fiber"
 import './App.css'
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 const Cube = ({positon, size, color}) => {
   const ref = useRef()
 
+  const [isHovered, setIsHovered] = useState(false)
+  const [isClicked, setIsClicked] = useState(false)
+
   useFrame((state, delta) => {
-    ref.current.rotation.x += delta
+    // ref.current.rotation.x += delta
+    ref.current.rotation.y += delta
+    // ref.current.rotation.z += delta
+    // ref.current.position.z = Math.sin(state.clock.elapsedTime)
   })
   return (
-    <mesh position={positon} ref={ref}>
+    <mesh 
+      position={positon} 
+      ref={ref} 
+      onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={() => setIsHovered(false)}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 1.5 : 1}
+    >
       <boxGeometry args={size}/>
-      <meshStandardMaterial color={color}/>
+      <meshStandardMaterial color={isHovered ? "orange" : "pink"}/>
     </mesh>
   )
 }
@@ -22,7 +35,7 @@ const App = () => {
       <Canvas>
         <directionalLight position={[0,0,2]} />
         <ambientLight />
-        <Cube positon={[0,1,0]} color={"orange"} size={[1,1,1]}/>
+        <Cube positon={[0,0,1]} color={"pink"} size={[1,1,1]}/>
       </Canvas>
     </>
   )
